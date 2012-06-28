a8409cc16e958bdf925d1666e96c4a4b94ba1168
Curso de E. Propias <campus60@ei140178.(none)>
2012-06-28 10:41:01 +0000

    he añadido las imagenes para el juego

--- faleteguapo.py
+++ faleteguapo.py
@@ -1,16 +1,24 @@
 import pygame
 import sys
-
+import time
 #Iniciar pygame
 pygame.init()
 
 # Configurar la ventana
 ancho= 1000
-alto= 1000
+alto= 1000 
 ventana = pygame.display.set_mode((ancho,alto))
 pygame.display.set_caption("EL CHICO DE LA NAVAJA ESTA BUENISIMO")
 
+#Cambiar de colores
+ROJO = (255 , 0, 0)
+NEGRO = (0, 0, 0)
+COLORES = (ROJO, NEGRO)
 
+for rojo in COLORES:
+	ventana.fill(rojo)
+	time.sleep(2)
+	pygame.display.update()
   # Bucle de eventos
 while True:
     for event in pygame.event.get():
