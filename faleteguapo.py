53245c28d674c9b8f23e873dd6ca346e8c41bd36
Curso de E. Propias <campus60@ei140178.(none)>
2012-07-02 10:50:43 +0000

    muñeco

--- faleteguapo.py
+++ faleteguapo.py
@@ -5,8 +5,8 @@ import time
 pygame.init()
 
 # Configurar la ventana
-ancho= 1000
-alto= 1000 
+ancho= 745	
+alto= 560
 ventana = pygame.display.set_mode((ancho,alto))
 pygame.display.set_caption("EL CHICO DE LA NAVAJA ESTA BUENISIMO")
 
@@ -17,11 +17,33 @@ COLORES = (ROJO, NEGRO)
 
 for rojo in COLORES:
 	ventana.fill(rojo)
-	time.sleep(2)
+	time.sleep(2) 
 	pygame.display.update()
+ 
+imagen = pygame.image.load ("img/pistas.jpg")
+imagen = imagen.convert()
+x,y = (0,0) 
+ventana.blit(imagen, [x,y])
+pygame.display.flip()
+
+imagen = pygame.image.load ("img/atleta.jpg") 
+imagen = imagen.convert()
+x,y = (200,200)
+ventana.blit(imagen, [x,y])
+pygame.display.flip()
   # Bucle de eventos
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit() 
+        if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				x = x - 10
+				ventana.blit(imagen, [x,y])
+				pygame.display.flip()
+        if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				x = x + 10
+				ventana.blit(imagen, [x,y])
+				pygame.display.flip()
