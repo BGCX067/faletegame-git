539cac151eb0c2ac16e98c3c832cc23ec5a49edd
Curso de E. Propias <campus60@ei140178.(none)>
2012-07-04 10:50:01 +0000

    dd

--- faleteguapo.py
+++ faleteguapo.py
@@ -12,6 +12,8 @@ pygame.display.set_caption("EL CHICO DE LA NAVAJA ESTA BUENISIMO")
 
 #Cambiar de colores
 ROJO = (255 , 0, 0)
+BLANCO = (255,255,255)
+AZUL = (0,0,255)
 NEGRO = (0, 0, 0)
 COLORES = (ROJO, NEGRO)
 
@@ -30,6 +32,25 @@ personaje = pygame.image.load ("img/atleta.jpg")
 personaje = personaje.convert()
 x_personaje,y_personaje = (200,200)
 pygame.display.flip()
+ 
+#Configurar el tipo de letra
+tamanio = 48
+fuente = pygame.font.SysFont(None, tamanio)
+ 
+#Configurar el texto
+texto = fuente.render('Hello world', True, BLANCO, AZUL)
+textoRect = texto.get_rect()
+textoRect.centerx = ventana.get_rect().centerx
+textoRect.centery = ventana.get_rect().centery
+ventana.blit(texto, textoRect)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   # Bucle de eventos
 while True:
     for event in pygame.event.get():
